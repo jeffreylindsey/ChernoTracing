89eaaf7184f01fc8f65901f1e3431025c500a667
jeffreylindsey <jeffrey@jeffreylindsey.com>
2022-06-28 16:41:00 -0700

    Implemented ray sphere intersection (mostly) as presented in episode 3.

--- ChernoTracing/src/Renderer.cpp
+++ ChernoTracing/src/Renderer.cpp
@@ -35,10 +35,37 @@ void c_Renderer::Render(Walnut::Image& r_Image)
 /*===========================================================================*/
 s_RGBA c_Renderer::RenderPixel(const glm::vec2& PixelUV)
 {
+	const glm::vec3 RayOrigin(0.0f, 0.0f, 2.0f);
+	const glm::vec3
+		RayDirection(PixelUV.x * 2.0f - 1.0f, PixelUV.y * 2.0f - 1.0f, 1.0f);
+
+	// Sphere
+	bool HitSphere = false;
+	{
+		constexpr float Radius = 0.5f;
+
+		const float a = glm::dot(RayDirection, RayDirection);
+		const float b = 2.0f * glm::dot(RayOrigin, RayDirection);
+		const float c = glm::dot(RayOrigin, RayOrigin) - Radius * Radius;
+
+		const float Discriminant = b * b - 4.0f * a * c;
+
+		HitSphere = (Discriminant >= 0.0f);
+	}
+
 	s_RGBA Result;
-	Result.Channel.R = static_cast<uint8_t>(PixelUV.x * 255);
-	Result.Channel.G = static_cast<uint8_t>(PixelUV.y * 255);
-	Result.Channel.B = 0;
+	if (HitSphere)
+	{
+		Result.Channel.R = 0;
+		Result.Channel.G = 0;
+		Result.Channel.B = 255;
+	}
+	else
+	{
+		Result.Channel.R = static_cast<uint8_t>(PixelUV.x * 255);
+		Result.Channel.G = static_cast<uint8_t>(PixelUV.y * 255);
+		Result.Channel.B = 0;
+	}
 	Result.Channel.A = 255;
 
 	return Result;
